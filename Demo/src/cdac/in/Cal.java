dcd8b184a82ef1171677773d40682bea14478938
Dev-Ashok <Dev-Ashok@gmail.com>
2023-05-26 14:00:08 +0530

    Created java project

---
+++ Demo/src/cdac/in/Cal.java
@@ -0,0 +1,15 @@
+package cdac.in;
+
+public class Cal {
+
+	public static void main(String[] args) {
+		int sum1 = 100 + 50;
+	    int sum2 = sum1 + 250;
+	    int sum3 = sum2 + sum2;
+	    System.out.println(sum1);
+	    System.out.println(sum2);
+	    System.out.println(sum3);
+
+	}
+
+}
